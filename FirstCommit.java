f455a63b072c24c2eaa58dbdcb43e4afec5ecd5a
vasanthkumarj2002 <vasanth22722@gmail.com>
2023-06-01 13:56:27 +0530

    Added the another statement

--- FirstCommit.java
+++ FirstCommit.java
@@ -1,7 +1,8 @@
 public class FirstCommit
 {
-public static void main (String []args)
+public static void main(String []args)
 {
 System.out.println("Hello World");
+System.out.println("I learn java full stack developer");
 }
 }
